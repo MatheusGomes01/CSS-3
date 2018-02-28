1b2bde84a1e28273c5276a1edd292fa944ff457d
matheus.ferreira <matheus.ferreira@lbca.com.br>
2018-02-28 14:38:52 -0300

    animação em CSS 3

---
+++ animação.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	
+			 <meta charset="utf-8">
+	
+		  <!-- Normalize -->
+		  <link rel="stylesheet" type="text/css" href="normalize.css">
+		
+		  <!-- Estilo customizado -->
+		  <link rel="stylesheet" type="text/css" href="style_animação.css">
+	</head>
+	<body>
+
+	<div id="box-animação">
+		
+	</div>
+
+	</body>
+</html>
